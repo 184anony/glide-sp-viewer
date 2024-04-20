78befa9c0427eaef2fb9af1d8e8410584208bf4b
184anony <ochiken184@gmail.com>
2024-04-20 19:34:11 +0900

    viewportの変更機能を追加

--- background.js
+++ background.js
@@ -1,16 +1,14 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function (details) {
-        const newHeaders = details.requestHeaders.map((header) => {
-            if (header.name.toLowerCase() === "user-agent") {
-                return {
-                    name: "User-Agent",
-                    value: "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
-                };
+        var headers = details.requestHeaders;
+        for (var i = 0; i < headers.length; ++i) {
+            if (headers[i].name === "User-Agent") {
+                headers[i].value =
+                    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1";
             }
-            return header;
-        });
-        return { requestHeaders: newHeaders };
+        }
+        return { requestHeaders: headers };
     },
     { urls: ["*://*.glide.page/*"] },
-    ["requestHeaders"]
+    ["blocking", "requestHeaders"]
 );
---
+++ content.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var metaTag = document.createElement("meta");
+    metaTag.name = "viewport";
+    metaTag.content = "width=375, initial-scale=1.0, user-scalable=no";
+    document.getElementsByTagName("head")[0].appendChild(metaTag);
+});
